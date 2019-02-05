fd16506a6204eb2a4166f1ee17aef4cf21f9a074
alemaulou <alemau@uw.edu>
2019-02-04 20:15:23 -0800

    Geometric rose

--- geometricRose/sketch.js
+++ geometricRose/sketch.js
@@ -10,7 +10,7 @@ function centerCanvas() {
 function setup() {
   cv = createCanvas(800, 800);
   centerCanvas();
-  slider = createSlider(1,5,4,0.1);
+  slider = createSlider(1,16,4,0.1);
 }
 
 function windowResized() {
@@ -21,6 +21,34 @@ function draw() {
   k = slider.value();
   background('#000');
   translate(width/2, height/2);
+
+  beginShape();
+  stroke(255);
+  noFill();
+  strokeWeight(1);
+  for(var a = 0; a < TWO_PI * 10; a+= 0.02) {
+    var r = 50 * cos(k* a);
+    var x = r*cos(a);
+    var y = r*sin(a);
+    vertex(x,y);
+  }
+
+  endShape();
+
+
+  beginShape();
+  stroke(255);
+  noFill();
+  strokeWeight(1);
+  for(var a = 0; a < TWO_PI * 10; a+= 0.02) {
+    var r = 200 * cos(k* a);
+    var x = r*cos(a);
+    var y = r*sin(a);
+    vertex(x,y);
+  }
+
+  endShape();
+
 
   beginShape();
   stroke(255);
